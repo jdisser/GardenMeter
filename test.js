823e9219f0df81a621376292322fd9a240bbab3a
jrdisser <jimdisser@altocirrusapps.com>
2020-07-18 20:02:55 -0600

    processes Decagon sensor output string

--- test.js
+++ test.js
@@ -13,30 +13,56 @@ const port = new SerialPort('/dev/serial0',{
     baudRate: 1200
 });
 
-const parser = port.pipe(new DelimiterParser ({
-    delimiter: '\r'
-}));
+const parser = port.pipe(new DelimiterParser ({delimiter: '\n'}));
 
 port.on("error", function (err) {
     console.log("Open Error: " + err.message)
 });
 
-/*
-port.write("Hello Serial Port World!!!\r\n", function(err){
-    if(err) {
-        return console.log("Write error: " + err.message);
-    }
-    console.log("Message Written!!");
-});
-*/
+var tempFn = function (senseT) {
+    return (senseT - 400)/10;
+};
 
+var conductFn = function (senseC) {
+    return 5 * (senseC - 700) + 700;
+};
+
+var vwcFn = function (senseV) {
+    return 4.3e-6 * Math.pow(senseV, 3) - 5.5e-4 * Math.pow(senseV, 2) + 2.92e-2 * senseV - 5.3e-2;
+}
+
+var convert = function (dataArray) {
+
+    var result = [];
+
+    result[0] = vwcFn(dataArray[0]);
+    result[1] = conductFn(dataArray[1]);
+    result[2] = tempFn(dataArray[2]);
+
+    return result;
+
+};
 
 parser.on("data", function (data){
+
     //strip junk characters from start of string
     var raw = data.toString();
     var result = raw;
     var raw = raw.slice(result.search(/[a-zA-Z0-9]/))
-    console.log("Data Rx: " + raw );
+    console.log("Data Raw: " + raw );
+
+    //create data array
+    result = raw.split(' ');
+    console.log("Split: " + result);
+    var data = result.map((str) => parseInt(str, 10));
+    console.log("Data: " + data);
+
+    //convert raw data to sensor output
+    var output = convert(data);
+    console.log("Volumetric Water Content: " + output[0]);
+    console.log("Conductivity: " + output[1]);
+    console.log("Temperature: " + output[2]);
+
 }).on("error", function (error){
      console.log("Error: " + error)
 });
